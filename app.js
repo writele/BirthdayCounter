abc0eb3b3cef4db7a883377155ce7d708c1cdfe8
Ele <ecpalmer21@gmail.com>
2016-10-06 15:18:45 -0400

    logic for getting difference between today and bday

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ $(document).ready(function () {
         var month = $("#month").val();
         var day = $("#day").val();
 
-        console.log(month);
-        console.log(day);
+        // Get difference in days between future birthday date and today
+        var today;
+        var birthday;
+
+        var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+        var firstDate = new Date(2008,01,12);
+        var secondDate = new Date(2008,01,22);
+
+        var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
     }
 
     $("#getDays").click(getDays);
