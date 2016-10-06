91142d0543f0958480d0b98db1759dc036b4e1c1
Ele <ecpalmer21@gmail.com>
2016-10-06 15:10:56 -0400

    Get month  and day as html elements

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-﻿
+﻿// Get user input from HTML page
+// Do something with that input
+// Attach output to HTML page
+
+$(document).ready(function () {
+    function getDays() {
+        var month = $("#month");
+        var day = $("#day");
+
+        console.log(month);
+        console.log(day);
+    }
+
+    $("#getDays").click(getDays);
+
+
+});
